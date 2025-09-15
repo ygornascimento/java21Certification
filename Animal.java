15e161f54c057c0fe840f422f129ec7d58a0ea2f
Ygor Nascimento <ygo.nascimento@outlook.com>
2025-09-15 06:42:17 -0300

    Chapter01 - Class and Source Files.

---
+++ Animal.java
@@ -0,0 +1,17 @@
+public class Animal {
+	String name;
+	
+	public Animal(String name) {
+	  this.name = name;
+	}
+	
+	public static void main(String args[]) {
+	Animal myAnimal = new Animal("MyAnimal2");
+	  System.out.println(myAnimal.name);
+	}
+	
+}
+
+class Animal2 {
+	String name;
+}
